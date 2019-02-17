701d91f6449f314d37da298886600f405a37ce40
Sitora Urazova <sitoraurazova@gmail.com>
2019-02-17 22:23:05 +0000

    Added to content

--- my-app/src/Main.js
+++ my-app/src/Main.js
@@ -1,5 +1,16 @@
 import React, { Component } from "react";
 
+import {
+  Route,
+  NavLink,
+  HashRouter
+}
+from "react-router-dom";
+
+import Home from "./Home";
+import About from "./About";
+import Content from "./Content";
+
 class Main extends Component {
   render() {
     return (
