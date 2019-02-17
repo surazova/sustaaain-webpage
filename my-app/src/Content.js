f6d6677f5f7fc6b1757d4d2dcd951dcd4c74b48f
Sitora Urazova <sitoraurazova@gmail.com>
2019-02-17 22:20:47 +0000

    Added content

---
+++ my-app/src/Content.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Content extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Content</h2>
+        <p> Your content here</p>
+        </div>
+    );
+  }
+}
+
+export default Content;
