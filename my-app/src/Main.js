dac7b18a3c697e6a9c9373d7a681303da5d31300
Sitora Urazova <sitoraurazova@gmail.com>
2019-02-17 22:03:20 +0000

    Edit

---
+++ my-app/src/Main.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <h1>SUSTAAAIN.</h1>
+        <ul className="header">
+          <li><a href="/">Home</a></li>
+          <li><a href="/About">About</a></li>
+          <li><a href="/Content">Content</a></li>
+        </ul>
+        <div className="content">
+        
+        </div>
+      </div>
+    )
+  }
+}
