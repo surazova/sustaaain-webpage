774d0db40eb40d0f6174ba46c20b08fa2f349062
Sitora Urazova <sitoraurazova@gmail.com>
2019-02-17 21:58:37 +0000

    Edit

---
+++ my-app/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./Main";
+
+// Call to render the Main component 
+// React Router helps you choose which components to show and which to hide 
+// Makes navigating look seamless
+ReactDOM.render(
+  <Main/>,
+  document, getElementById("root")
+);
